d02a1dfb0853ce60a47bdb50befc78004f48e842
Ilham Akbar <ilhamije@gmail.com>
2024-11-05 18:42:57 +0000

    demo prediction with inferencer

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -6,7 +6,7 @@ env_cfg = dict(
     dist_cfg=dict(backend='nccl'),
     gpus=2,
 )
-vis_backends = [dict(type='LocalVisBackend')]
+vis_backends = [dict(type='LocalVisBackend'), dict(type='TensorboardVisBackend')]
 visualizer = dict(
     type='SegLocalVisualizer', vis_backends=vis_backends, name='visualizer')
 log_processor = dict(by_epoch=False)
